10421362750e1f229459e2d5cf44c2d3a69bdd47
iorn5891 <mvufys22u@yahoo.co.jp>
2024-02-26 20:33:08 +0900

    コミットテスト

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,14 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import Label from '@/Components/InputLabel.vue';
+import Input from '@/Components/TextInput.vue';
+
+const emitTest = e => console.log(e);
+</script>
+
+<template>
+    <GuestLayout>
+        <Label value="件名">タイトル</Label>
+        <Input model-value="place holder" @update:model-value="emitTest"></Input>
+    </GuestLayout>
+</template>
